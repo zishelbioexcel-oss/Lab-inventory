b815cf664f5123687bea2437aa94dd374ac3c8c8
zishelbioexcel-oss <zishelbio.excel@gmail.com>
2025-12-16 16:21:46 +0900

    Update inventory_app.py

--- inventory_app.py
+++ inventory_app.py
@@ -1,5 +1,3 @@
-Python
-
 import streamlit as st
 import gspread
 import json
@@ -241,4 +239,5 @@ with tab3:
         # (선택 사항) 입출고 히스토리 보기
         with st.expander("📜 상세 입출고 이력 보기"):
             st.dataframe(df_log.sort_values(by=df_log.columns[0], ascending=False), use)
+
 
